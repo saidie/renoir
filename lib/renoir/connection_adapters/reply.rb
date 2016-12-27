9f9822945c99c9370143ea0bf6414c7d3e295c16
Hiroshi Saito <saito.die@gmail.com>
2016-12-27 18:30:22 +0900

    Add Renoir::ConnectionAdapters::Reply
    It encapsulates a reply from backend Redis connection for client

---
+++ lib/renoir/connection_adapters/reply.rb
@@ -0,0 +1,27 @@
+module Renoir
+  module ConnectionAdapters
+    module Reply
+      class Base
+        attr_reader :cause
+
+        def initialize(cause)
+          @cause = cause
+        end
+      end
+
+      class RedirectionError < Base
+        attr_reader :ask, :ip, :port
+
+        def initialize(cause, ask, ip, port)
+          super(cause)
+          @ask = ask
+          @ip = ip
+          @port = port
+        end
+      end
+
+      class ConnectionError < Base
+      end
+    end
+  end
+end
