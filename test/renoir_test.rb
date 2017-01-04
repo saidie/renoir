fe2c03f9c494524bd7cebea0c0cc2daaa6ed688d
Hiroshi Saito <saito.die@gmail.com>
2017-01-04 17:17:51 +0900

    Remove scaffold tests

--- test/renoir_test.rb
+++ test/renoir_test.rb
@@ -1,11 +1,4 @@
 require 'test_helper'
 
 class RenoirTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Renoir::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
