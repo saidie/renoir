080212545bb47cf4d09ba8cf4f5e1f18d5fbb720
Hiroshi Saito <saito.die@gmail.com>
2017-01-04 21:21:38 +0900

    Add config file for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'renoir'
 
+require 'yaml'
 require 'minitest/autorun'
+
+CONFIG = YAML.load(open(File.expand_path('../config.yml', __FILE__)))
