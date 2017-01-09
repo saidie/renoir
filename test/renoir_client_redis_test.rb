fd006a11042e0935da4a35361ce20bd9d540e2ad
Hiroshi Saito <saito.die@gmail.com>
2017-01-09 18:04:23 +0900

    Add test cases for Renoir::Client#pipelined

--- test/renoir_client_redis_test.rb
+++ test/renoir_client_redis_test.rb
@@ -32,6 +32,23 @@ class RenoirClientRedisTest < Minitest::Test
     assert res.size == 3
   end
 
+  def test_pipelined
+    assert_raises RuntimeError do
+      @client.pipelined do |tx|
+        tx.get('hoge')
+        tx.info
+        tx.get('fuga')
+      end
+    end
+
+    res = @client.pipelined do |tx|
+      tx.get('hoge{1}')
+      tx.info
+      tx.get('fuga{1}')
+    end
+    assert res.size == 3
+  end
+
   def test_call_single_key_command
     assert @client.set('hoge', 123) == 'OK'
     assert @client.set('fuga', 'piyo') == 'OK'
